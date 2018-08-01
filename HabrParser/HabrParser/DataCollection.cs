de98b18bacb91b35b51464344b62e3fd4bdd710a
Egor78797978 <41723606+Egor78797978@users.noreply.github.com>
2018-08-01 14:04:51 +0300

    Библиотеки
    Удалено подключение лишних библиотек

--- HabrParser/HabrParser/DataCollection.cs
+++ HabrParser/HabrParser/DataCollection.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 using HtmlAgilityPack;
-using System.Windows.Forms;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Globalization;
 
 
